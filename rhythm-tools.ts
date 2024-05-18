849b7c3f86c417954f624052dc85645f1faa3a8f
urassari <136824813+urassari@users.noreply.github.com>
2024-05-18 13:36:05 +0300

    Update rhythm-tools.ts

--- rhythm-tools.ts
+++ rhythm-tools.ts
@@ -6,40 +6,28 @@ let right = 0
 let down = 0
 let score = 0
 forever(function () {
-    if (controller.A.isPressed()) {
-        if (a != 0) {
-            a = 0
-        }
+    if (controller.A.isPressed() && a != 0) {
+        a = 0
     } else { a += 1 }
-    if (controller.B.isPressed()) {
-        if (b != 0) {
-            b = 0
-        }
+    if (controller.B.isPressed() && b != 0) {
+        b = 0
     } else { b += 1 }
-    if (controller.left.isPressed()) {
-        if (left != 0) {
-            left = 0
-        }
+    if (controller.left.isPressed() && left != 0) {
+        left = 0
     } else { left += 1 }
-    if (controller.right.isPressed()) {
-        if (right != 0) {
-            right = 0
-        }
+    if (controller.right.isPressed() && right != 0) {
+        right = 0
     } else { right += 1 }
-    if (controller.up.isPressed()) {
-        if (up != 0) {
-            up = 0
-        }
+    if (controller.up.isPressed() && up != 0) {
+        up = 0
     } else { up += 1 }
-    if (controller.down.isPressed()) {
-        if (down != 0) {
-            down = 0
-        }
+    if (controller.down.isPressed() && down != 0) {
+        down = 0
     } else { down += 1 }
     pause(1)
 })
 
-//% color="#AA278D" weight=100 icon="\f2f2"
+//% color="#AA278D" weight=100 icon="\f017"
 namespace rhythmTools {
 
     //% block
